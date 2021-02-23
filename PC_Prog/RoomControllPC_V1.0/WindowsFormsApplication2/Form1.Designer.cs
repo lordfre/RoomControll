296a4e7c0b0cee8f8b81bcb179aca733e70b9da1
lordfre <77280225+lordfre@users.noreply.github.com>
2021-02-23 13:29:50 +0300

    21

---
+++ PC_Prog/RoomControllPC_V1.0/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 68);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(383, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 133);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(383, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Visible = false;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update COM List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(116, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(82, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Port Select";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 21);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(116, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 21);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Light Off All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(270, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 21);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "On/Off12V";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(343, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 21);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "On/Off5V";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(197, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(67, 21);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "On/Off24V";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 40);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(98, 21);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.Text = "Mode";
+            this.comboBox2.Visible = false;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Soft Box 1";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(284, 13);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 21);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Recet Controll";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.BackColor = System.Drawing.Color.Red;
+            this.trackBar4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.trackBar4.Location = new System.Drawing.Point(16, 67);
+            this.trackBar4.Maximum = 255;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar4.Size = new System.Drawing.Size(383, 45);
+            this.trackBar4.TabIndex = 18;
+            this.trackBar4.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar4.Visible = false;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.BackColor = System.Drawing.Color.Lime;
+            this.trackBar5.Location = new System.Drawing.Point(16, 163);
+            this.trackBar5.Maximum = 255;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(383, 45);
+            this.trackBar5.TabIndex = 19;
+            this.trackBar5.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar5.Visible = false;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.BackColor = System.Drawing.Color.Blue;
+            this.trackBar3.Location = new System.Drawing.Point(16, 115);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(383, 45);
+            this.trackBar3.TabIndex = 20;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar3.Visible = false;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Soft Box 2";
+            this.label1.Visible = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(17, 68);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(144, 13);
+            this.linkLabel1.TabIndex = 23;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "РЕПОЗИТОРИЙ НА GitHub";
+            this.linkLabel1.Visible = false;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 13);
+            this.progressBar1.Maximum = 10000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(266, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 24;
+            this.progressBar1.Visible = false;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 215);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Room Controll";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PC_Prog/RoomControllPC_V1.0/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        bool isConnected = false;
+        SerialPort port;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox2.Items.Add("None");
+            comboBox2.Items.Add("Light Soft Box");
+            comboBox2.Items.Add("RGB");
+            comboBox2.Items.Add("References");
+            this.Size = new System.Drawing.Size(300,80);
+            
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void connectToArduino()
+        {
+            isConnected = true;
+            string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+            port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            button2.Text = "Disconnect";
+            button3.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = true;
+            comboBox2.Visible = true;
+            this.Size = new System.Drawing.Size(429, 110);
+        }
+
+        private void disconnectFromArduino()
+        {
+            isConnected = false;
+            port.Close();
+            button2.Text = "Connect";
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            comboBox2.Visible = false;
+            trackBar2.Visible = false;
+            trackBar1.Visible = false;
+            trackBar3.Visible = false;
+            trackBar4.Visible = false;
+            trackBar5.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            comboBox2.Visible = false;
+            linkLabel1.Visible = false;
+            this.Size = new System.Drawing.Size(300, 80);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            // Получаем список COM портов доступных в системе
+            string[] portnames = SerialPort.GetPortNames();
+            // Проверяем есть ли доступные
+            if (portnames.Length == 0)
+            {
+                MessageBox.Show("COM PORT not found");
+            }
+            foreach (string s in portnames)
+            {
+                //добавляем доступные COM порты в список              
+                comboBox1.Items.Add(s);
+                
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                connectToArduino();
+            }
+            else
+            {
+                disconnectFromArduino();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            port.Write("#R|");
+            disconnectFromArduino();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string vis = comboBox2.GetItemText(comboBox2.SelectedItem);
+            if(vis == "None")
+            {
+                this.Size = new System.Drawing.Size(429, 110);
+                trackBar2.Visible = false;
+                trackBar1.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                trackBar3.Visible = false;
+                trackBar4.Visible = false;
+                trackBar5.Visible = false;
+                linkLabel1.Visible = false;
+            }
+            if (vis=="RGB")
+            {
+                trackBar2.Visible=false;
+                trackBar1.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                trackBar3.Visible = true;
+                trackBar4.Visible = true;
+                trackBar5.Visible = true;
+                linkLabel1.Visible = false;
+                this.Size = new System.Drawing.Size(429, 255);
+            }
+            else if (vis == "Light Soft Box")
+            {
+                trackBar2.Visible = true;
+                trackBar1.Visible = true;
+                label1.Visible = true;
+                label2.Visible = true;
+                trackBar3.Visible = false;
+                trackBar4.Visible = false;
+                trackBar5.Visible = false;
+                linkLabel1.Visible = false;
+                this.Size = new System.Drawing.Size(429, 255);
+            }
+            else if (vis == "References")
+            {
+                trackBar2.Visible = false;
+                trackBar1.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                trackBar3.Visible = false;
+                trackBar4.Visible = false;
+                trackBar5.Visible = false;
+                linkLabel1.Visible = true;
+                this.Size = new System.Drawing.Size(429, 126);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            port.Write("#OA|");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            port.Write("#V24|");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            port.Write("#V12|");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            port.Write("#V5|");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            String LO = "#LO";
+            LO = LO + Convert.ToString(trackBar1.Value, 16);
+            LO = LO + "|";
+            port.Write(LO);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            String LP = "#LP";
+            LP = LP + Convert.ToString(trackBar2.Value, 16);
+            LP = LP + "|";
+            port.Write(LP);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            String LG = "#LG";
+            LG = LG + Convert.ToString(trackBar5.Value, 16);
+            LG = LG + "|";
+            port.Write(LG);
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            String LR = "#LR";
+            LR = LR + Convert.ToString(trackBar4.Value, 16);
+            LR = LR + "|";
+            port.Write(LR);
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            String LB = "#LB";
+            LB = LB + Convert.ToString(trackBar3.Value, 16);
+            LB = LB + "|";
+            port.Write(LB);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/lordfre/RoomControll");
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
